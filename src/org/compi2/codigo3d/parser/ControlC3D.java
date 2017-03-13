1759d0278ca44a75424d8973ef591c5afb1c57cf
568robin8 <abr568@gmail.com>
2017-03-12 21:35:41 -0600

    Hoja de trabajo no.3 Nombre: Robin Armando Salvatierra Bautista Carnet: 200915428

--- src/org/compi2/codigo3d/parser/ControlC3D.java
+++ src/org/compi2/codigo3d/parser/ControlC3D.java
@@ -8,6 +8,9 @@ public class ControlC3D {
     
     private static int temp = 0;
     private static String c3d = "";
+    private static int etq = 0;
+    private static String etq_v = "";
+    private static String etq_f = "";
     
     /**
      * Reinicia las variables estáticas relacionadas con la generación del
@@ -16,6 +19,9 @@ public class ControlC3D {
     public static void reiniciar(){
         temp = 0;
         c3d = "";
+        etq = 0;
+        etq_v ="";
+        etq_f = "";
     }
     
     /**
@@ -26,6 +32,10 @@ public class ControlC3D {
         return "t$"+temp++;
     }
     
+    
+     public static String generar_etiqueta(){
+        return "L"+etq++;
+    }
     /**
      * Agrega la sentencia que recibe como parámetro a la cadena de código
      * de tres direcciones que se va generando hasta ese momento.
@@ -39,8 +49,30 @@ public class ControlC3D {
      * Devuelve el código 3D generao hasta el momento de su invocación.
      * @return Código 3D generado
      */
-    public static String getC3D(){
-        return c3d;
+        public static String getC3D(){
+        if(etq_v.equals("")||etq_f.equals("")){
+            c3d += "Ultimo valor: t"+(temp-1);
+            return c3d;
+        }else{
+            c3d += "Etiqueta(s) Verdadero: " + etq_v+"\n";
+            c3d += "Etiqueta(s)  Falso: " + etq_f +"\n";
+            return c3d;
+        }
     }
     
+    
+        public static void agregar_Verdadero(String e){
+        if(etq_v.equals("")){
+            etq_v = e;
+        }else{
+            etq_v += ","+e;
+        }
+    }
+    public static void agregar_Falso(String e){
+        if(etq_f.equals("")){
+            etq_f = e;
+        }else{
+            etq_f += ","+e;
+        }
+    }
 }
--- src/org/compi2/codigo3d/parser/NodoC3D.java
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -8,13 +8,39 @@ package org.compi2.codigo3d.parser;
  */
 public class NodoC3D {
     private String cad;
+    private String etq_v;
+    private String etq_f;
 
     public NodoC3D(String cad) {
         this.cad = cad;
+        this.etq_v= "";
+        this.etq_f = "";
+    }
+    
+       public NodoC3D(String cad,String v, String f) {
+        this.cad = cad;
+        this.etq_v = v;
+        this.etq_f = f;
     }
     
     public String getCad(){
         return cad;
     }
         
+    public String get_ev() {
+        return etq_v;
+    }
+
+    public void set_ev(String e) {
+        this.etq_v = e;
+    }
+
+    public String get_ef() {
+        return etq_f;
+    }
+
+    public void set_ef(String e) {
+        this.etq_f = e;
+    }
+
 }
